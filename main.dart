79ab08f5c616eea885b6fcfa2f8057eeea1889a6
malumoreiras <vamptmari@gmail.com>
2026-08-04 12:42:55 -0300

    widget in flutter

--- main.dart
+++ main.dart
@@ -1,71 +1,31 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MyButtonApp());
 }
-//Classe Meu Aplicativo 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+
+class MyButtonApp extends StatelessWidget {
+  const MyButtonApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HelloWorldPage(),
-    );
-  }
-}
-
-class HelloWorldPage extends StatelessWidget {
-  const HelloWorldPage({super.key});
-
-@override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      // O corpo da tela é o único componente visível
-      body: Center(
-      child: Column(
-            children: [
-    
-              Text(
-                'Maria Luiza',
-                style: TextStyle(
-                  fontSize: 32,
-                  fontWeight: FontWeight.bold,
-                  color: Color(0xFFE67E22),
-                ),
-              ),
-
-              Text(
-                'SESI 400',
-                style: TextStyle(
-                  fontSize: 32,
-                  fontStyle: FontStyle.italic,
-                  color: Color(0xFF008736),
-                ),
-              ),
-            
-               
-              Text(
-                'São Roque',
-                style: TextStyle(
-                  fontSize: 32,
-                  fontWeight: FontWeight.normal,
-                  color: Color(0xFF360087),
-                ),
-              ),
-                
-              Text(
-                'Amanda',
-                style: TextStyle(
-                  fontSize: 32,
-                  fontWeight: FontWeight.normal,
-                  color: Color(0xFFE67E22),
-                ),
-              ),
-            ],
+      home: Scaffold(
+        body: Center(
+          child: ElevatedButton(
+            onPressed: () {
+              // Ação vazia
+            },
+            style: ElevatedButton.styleFrom(
+              backgroundColor: const Color(0xFF360087), // Cor de fundo
+              foregroundColor: Colors.blueAccent,            // Cor do texto
+              padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 15),
+            ),
+            child: const Text('Sou um Botão'),
+          ),
+        ),
       ),
-    ),
     );
   }
 }
